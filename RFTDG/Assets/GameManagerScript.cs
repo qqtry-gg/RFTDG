89ee777bf2618eb831d9facdc30826572a43e87f
qqtry-gg <wojtek.bielak1@interia.pl>
2025-12-19 17:30:06 +0100

    added Spawning Buttons

--- RFTDG/Assets/GameManagerScript.cs
+++ RFTDG/Assets/GameManagerScript.cs
@@ -4,6 +4,8 @@ public class GameManagerScript : MonoBehaviour
 {
     [SerializeField] Transform SpawnPlace;
     [SerializeField] GameObject Skeleton;
+    [SerializeField] GameObject Goblin;
+    [SerializeField] GameObject Wolf;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,6 +19,14 @@ public class GameManagerScript : MonoBehaviour
     }
     public void SpawnSkeleton()
     {
-        Instantiate(Skeleton, SpawnPlace);
+        Instantiate(Skeleton, SpawnPlace.position, Quaternion.identity);
+    }
+    public void SpawnGoblin()
+    {
+        Instantiate(Goblin, SpawnPlace.position, Quaternion.identity);
+    }
+    public void SpawnWolf()
+    {
+        Instantiate(Wolf, SpawnPlace.position, Quaternion.identity);
     }
 }
