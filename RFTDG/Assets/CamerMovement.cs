6476cd2197702520b6cbcc8a00c520b18a3a1588
qqtry-gg <wojtek.bielak1@interia.pl>
2025-12-19 20:38:20 +0100

    Added Camera Movement

---
+++ RFTDG/Assets/CamerMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class CamerMovement : MonoBehaviour
+{
+    [SerializeField] Camera camera;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll != 0)
+        {
+            camera.orthographicSize -= scroll;
+            camera.orthographicSize = Mathf.Clamp(camera.orthographicSize, 3f, 8.6f);
+        }
+        float movementVertical = Input.GetAxis("Vertical");
+        float movementHorizontal = Input.GetAxis("Horizontal");
+        Vector3 CurrentPosition = new Vector3(camera.transform.position.x, camera.transform.position.y, camera.transform.position.z);
+
+
+        CurrentPosition.y = camera.transform.position.y + (movementVertical / 10);
+        CurrentPosition.x = camera.transform.position.x + (movementHorizontal / 10);
+
+
+        CurrentPosition.y = Mathf.Clamp(CurrentPosition.y, -6f, 6f);
+        CurrentPosition.x = Mathf.Clamp(CurrentPosition.x, -10f, 10f);
+
+        camera.transform.position = CurrentPosition;
+
+    }
+}
