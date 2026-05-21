0cfc078cd1f7b76431c92c9eebf36f8daa7eb5ad
qqtry-gg <wojtek.bielak1@interia.pl>
2026-05-21 18:08:40 +0200

    spike spawn

--- RFTDG/Assets/Script/RNDSpawning.cs
+++ RFTDG/Assets/Script/RNDSpawning.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 public class RNDSpawning : MonoBehaviour
 {
     [SerializeField] List<Transform> spawns = new List<Transform>();
+    [SerializeField] List<Transform> spikeSpawns = new List<Transform>();
     [SerializeField] GameObject[] towers;
+    [SerializeField] GameObject[] spikeTowers;
     Transform currentSpawn;
+    Transform currentSpawnSpike;
     GameObject currentTower;
+    GameObject currentSpikeTower;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -28,6 +32,17 @@ public class RNDSpawning : MonoBehaviour
 
             Instantiate(currentTower, currentSpawn.transform.position, Quaternion.identity);
             spawns.Remove(currentSpawn);
+        }
+        if (Input.GetKeyDown(KeyCode.V) && spikeSpawns.Count > 0)
+        {
+            int randomSpawnInt2 = UnityEngine.Random.Range(0, spikeSpawns.Count);
+            currentSpawnSpike = spikeSpawns[randomSpawnInt2];
+
+            int randomTowerInt2 = UnityEngine.Random.Range(0, spikeTowers.Length);
+            currentSpikeTower = spikeTowers[randomTowerInt2];
+
+            Instantiate(currentSpikeTower, currentSpawnSpike.transform.position, Quaternion.identity);
+            spikeSpawns.Remove(currentSpawnSpike);
         }
     }
 }
