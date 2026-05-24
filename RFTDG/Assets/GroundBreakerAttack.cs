83bf4416610f81621ace47d690a196d095438811
qqtry-gg <wojtek.bielak1@interia.pl>
2026-05-24 21:16:15 +0200

    added new tower
    added new ground breaker tower

---
+++ RFTDG/Assets/GroundBreakerAttack.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class GroundBreakerAttack : MonoBehaviour
+{
+    Collider2D[] enemyColliders;
+    float timer = 0f;
+    [Header("Attributes")]
+    [SerializeField] LayerMask enemyLayerMask;
+    [SerializeField] float radius;
+    [SerializeField] float attackSpeed;
+    [SerializeField] float damage;
+    [SerializeField] Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= attackSpeed)
+        {
+            GroundBreakerAttackMethod();
+            timer = 0f;
+        }
+
+    }
+    void GroundBreakerAttackMethod()
+    {
+        enemyColliders = Physics2D.OverlapCircleAll(transform.position, radius, enemyLayerMask);
+        animator.SetTrigger("Attack");
+        foreach (Collider2D enemy in enemyColliders)
+        {
+            enemy.GetComponent<HealthScript>().TakeDamage(damage);
+        }
+
+    }
+       
+}
