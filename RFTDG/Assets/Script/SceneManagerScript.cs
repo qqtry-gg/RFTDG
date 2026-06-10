0fba2cfdb167351681475e09d2ee0fff20d56089
qqtry-gg <wojtek.bielak1@interia.pl>
2026-06-10 15:15:54 +0200

    made the possibilit to pause the game
    added menu for the game paused (buttons like exit, resume and settings)

--- RFTDG/Assets/Script/SceneManagerScript.cs
+++ RFTDG/Assets/Script/SceneManagerScript.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 
 public class SceneManagerScript : MonoBehaviour
 {
+    [SerializeField] GameObject gamePausedUI;
+
     public void ExitTheGame()
     {
         
@@ -28,6 +30,14 @@ public class SceneManagerScript : MonoBehaviour
     public void BackScene()
     {
         SceneManager.LoadScene(0);
+    }
+    public void OpenGamePausedUI()
+    {
+        gamePausedUI.SetActive(true);
+    }
+    public void CloseGamePausedUI()
+    {
+        gamePausedUI.SetActive(false);
     }
 
 }
