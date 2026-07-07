39aa209fa01b5b3cf0e991cd7bc19534a667e936
qqtry-gg <wojtek.bielak1@interia.pl>
2026-07-07 19:37:15 +0200

    added Volume Settings
    also the settings are saved by playerPrefs

--- RFTDG/Assets/VolumeSettings.cs
+++ RFTDG/Assets/VolumeSettings.cs
@@ -1,16 +1,57 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumeSettings : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] AudioMixer Mixer;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider SFXSlider;
+    private void Start()
     {
-        
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else SetMusicVolume();
+        if (PlayerPrefs.HasKey("SFXVolume"))
+        {
+            LoadSFX();
+        }
+        else SetSFXVolume();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void SetMusicVolume()
     {
-        
+        if (musicSlider != null)
+        {
+            float volume = musicSlider.value;
+            Mixer.SetFloat("music", Mathf.Log10(volume) * 20);
+            PlayerPrefs.SetFloat("musicVolume", volume);
+        }
+
+    }
+    public void SetSFXVolume()
+    {
+        if (SFXSlider != null)
+        {
+            float volume = SFXSlider.value;
+            Mixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+            PlayerPrefs.SetFloat("SFXVolume", volume);
+        }
+
+    }
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        Mixer.SetFloat("music", Mathf.Log10(PlayerPrefs.GetFloat("musicVolume")) * 20);
+
+        SetMusicVolume();
+    }
+    private void LoadSFX()
+    {
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        Mixer.SetFloat("SFX", Mathf.Log10(PlayerPrefs.GetFloat("SFXVolume")) * 20);
+
+        SetSFXVolume();
     }
 }
