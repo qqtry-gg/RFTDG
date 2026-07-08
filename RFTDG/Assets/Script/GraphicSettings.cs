a7d91e3be69fab56c2f01018e8a16dc28ae6a794
qqtry-gg <wojtek.bielak1@interia.pl>
2026-07-08 15:43:33 +0200

    added saving player preferences to the Graphic Quality Settings

--- RFTDG/Assets/Script/GraphicSettings.cs
+++ RFTDG/Assets/Script/GraphicSettings.cs
@@ -5,5 +5,17 @@ public class GraphicSettings : MonoBehaviour
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+        PlayerPrefs.SetInt("qualityLevel", qualityIndex);
+    }
+    private void Start()
+    {
+        LoadQualitySettings();
+    }
+    void LoadQualitySettings()
+    {
+        if (PlayerPrefs.HasKey("qualityLevel"))
+        {
+            QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("qualityLevel"));
+        }
     }
 }
