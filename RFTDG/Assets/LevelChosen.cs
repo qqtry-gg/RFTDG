641b57bff60d3ba0b194a52757511a2fdc835e2e
qqtry-gg <wojtek.bielak1@interia.pl>
2026-07-08 19:26:29 +0200

    fixed some issues with the current map

---
+++ RFTDG/Assets/LevelChosen.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class LevelChosen : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("Klikn¹³em przycisk lol, nazwa obiektu: " + gameObject.name);
+    }
+}
